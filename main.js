ac0276a50a936cc2881951a6603699465c4ab951
Graham McCarthy <graham@soapboxhq.com>
2020-02-20 14:08:24 -0500

    Electron App upgrade (#2)
    * upgrading to latest version of electron
    
    * updating logo and removing garbage
    
    * add google urls + height chagne
    
    * new gitignore
    
    * new gitignore
    
    * update icon
    
    * installed DMG creator
    
    * icons.
    
    * reload / devtools / keyboard shortcuts
    
    * this does not work.. :(
    
    * upgraded electron to latest version. + getting local storage to work
    
    * prettty
    
    * sign out clears token. CSS for drag and drop working properly. Fix soapboxURL.
    
    * logout now actually logs you out.
    
    removed unneeded console log messages
    
    * better spacing on topbar.
    
    * added notification badge and web notification
    
    * added macId
    
    * fix so that the notification doesn't pop in, if notification count has not chnaged between opens.
    
    * works with google signin. extended polling to 15 seconds.
    
    * update :arrow_up:
    
    * killed my node modules.
    
    * added cmd+forward and cmd+back functionality
    
    * hopefully fixes notifications build
    
    * fixes push notifications

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
-const { app, BrowserWindow, Menu, shell } = require('electron')
+const { app, BrowserWindow, Menu, shell } = require('electron');
+const Store = require('electron-store');
+const path = require('path');
+const url = require('url');
+const fetch = require('electron-fetch').default;
+const AsyncPolling = require('async-polling');
+const notifier = require('node-notifier');
 
-const path = require('path')
-const url = require('url')
+const store = new Store();
+const BASE_DOMAIN = "soapboxhq.com";
+const API_HOST = "api.goodtalk.soapboxhq.com";
 
 const electronLinks = [
   'https://slack.com/signin',
-  'https://slack.com/oauth'
+  'https://slack.com/oauth',
+  'https://accounts.google.com/o/oauth2',
+  'https://www.googleapis.com/oauth2',
+  'https://accounts.google.com/signin'
 ];
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+var unreadCount = 0;
 
 function createWindow () {
+
+  if (store.get('unreadCount')) {
+    unreadCount = store.get('unreadCount');
+  }
+
   // Create the browser window.
   mainWindow = new BrowserWindow({
     // titleBarStyle: 'hidden-inset',
     backgroundColor: '#f8f8f8',
-    width: 800,
-    height: 600,
-    minHeight: 500,
-    minWidth: 320,
+    titleBarStyle: 'hidden',
+    width: 1200,
+    height: 800,
+    minWidth: 660,
+    minHeight: 400,
     webPreferences: {
       javascript: true,
       plugins: true,
@@ -33,15 +50,10 @@ function createWindow () {
   });
 
   mainWindow.loadURL(url.format({
-    pathname: 'ideate.io',
+    pathname: getSoapboxURL(),
     protocol: 'https:',
-    // pathname: 'localhost:4200',
-    // protocol: 'http:',
     slashes: true
   }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 
   // Open links in the browser
   mainWindow.webContents.on('new-window', function(e, url) {
@@ -65,31 +77,80 @@ function createWindow () {
   });
 
   var template = [{
-        label: "Application",
-        submenu: [
-            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-            { type: "separator" },
-            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-        ]}, {
-        label: "Edit",
-        submenu: [
-            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-            { type: "separator" },
-            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-        ]}
-    ];
+      label: "Application",
+      submenu: [
+          { label: "About", selector: "orderFrontStandardAboutPanel:" },
+          { label: "Choose Soapbox", accelerator: "Shift+Command+C", 
+            click: function() {
+              mainWindow.loadURL(url.format({
+                pathname: 'app.soapboxhq.com',
+                protocol: 'https:',
+                slashes: true
+              }))
+            }},
+          { label: "Sign Out", accelerator: "Shift+Command+L", 
+            click: function() { 
+                    DeleteStoredData();
+                    mainWindow.webContents.session.clearStorageData();
+                    mainWindow.reload();
+            }},
+          { type: "separator" },
+          { label: "Quit", accelerator: "Command+Q", 
+            click: function() { 
+              app.quit(); 
+            }}
+      ]}, {
+      label: "Edit",
+      submenu: [
+          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]},{
+      label: 'View',
+      submenu: [
+        { label: 'Reload', accelerator: 'CmdOrCtrl+R', 
+          click(item, focusedWindow) { 
+            mainWindow.reload();
+          }},
+        { label: "Back", accelerator: "CmdOrCtrl+Left", click: function() { 
+            if(mainWindow.webContents.canGoBack()) {
+              mainWindow.webContents.goBack();
+            }
+          }},
+        { label: "Forward", accelerator: "CmdOrCtrl+Right", click: function() { 
+            if(mainWindow.webContents.canGoForward()) {
+              mainWindow.webContents.goForward();
+            }
+          }},   
+        { label: 'Toggle Dev Tools', accelerator: 'CmdOrCtrl+I', 
+          click(item, focusedWindow) { 
+            mainWindow.webContents.openDevTools(); 
+          }}
+      ]}
+  ];
 
-    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  AsyncPolling(function (end) {
+    updateUnreadBadgeCount();
+    // Then notify the polling when your job is done:
+    end();
+    // This will schedule the next call.
+  }, 15000).run();
+  
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
+
+app.setAppUserModelId("com.soapboxhq.soapbox-desktop-app");
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function ()  {
+  createWindow()
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -98,7 +159,7 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
@@ -106,7 +167,77 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})
+  updateUnreadBadgeCount();
+});
+
+function DeleteStoredData() {
+  store.delete('soapboxUrl');
+  store.delete('me');
+  store.delete('token');
+}
+
+function getSoapboxURL() {
+  var url = 'app.soapboxhq.com';
+  
+  var token = store.get('token');
+  var soapboxUrl = store.get('soapboxUrl');
+
+  if(soapboxUrl && token) {
+    url = soapboxUrl.replace(/(^\w+:|^)\/\//, '');
+  }
+
+  return url;
+}
+
+function updateUnreadBadgeCount(){
+  var token = store.get('token');
+  var soapboxUrl = store.get('soapboxUrl');
+  
+  if (token && soapboxUrl) {
+    var options = {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+    }
+
+    var localUnreadCount = 0;
+    var apiURL = `https://${API_HOST}/channels`;
+    fetch(apiURL, options)
+      .then(res => res.json())
+      .then(json => {
+        json.data.forEach(function(channel){
+          var attributes = channel.attributes
+          if(attributes['is-read'] != undefined && attributes['is-read'] == false) {
+            localUnreadCount++;
+          }
+        });
+
+        if(localUnreadCount > unreadCount) {
+          NotifyUserOfUnreadChannels();
+        }
+
+        unreadCount = localUnreadCount;
+        app.badgeCount = unreadCount;
+        store.set('unreadCount', unreadCount);
+      }).catch(function() {
+        console.log("error");
+      });
+  } else {
+    app.badgeCount = unreadCount;
+  }
+}
+
+function NotifyUserOfUnreadChannels() {
+  let iconAddress = path.join(__dirname, '/assets/icons/mac/icon.icns');
+  notifier.notify({
+    title: 'New unread items! 📫 ',
+    message: 'You have new unread channels on Soapbox. Check them out now.',
+    icon: iconAddress,
+    appName: "com.soapboxhq.soapbox-desktop-app",
+  });
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- preload.js
+++ preload.js
@@ -1,9 +1,14 @@
 /**
  Preload file that will be executed in the renderer process
  */
-const {webFrame, ipcRenderer} = require('electron')
-const path = require('path')
-const fs = require('fs')
+const {webFrame, ipcRenderer} = require('electron');
+const Store = require('electron-store');
+const path = require('path');
+const fs = require('fs');
+
+const store = new Store();
+const BASE_DOMAIN = "soapboxhq.com";
+const API_HOST = "api.goodtalk.soapboxhq.com";
 
 setNotificationCallback((title, opt) => {
     ipcRenderer.send('notification', title, opt);
@@ -27,7 +32,6 @@ ipcRenderer.on('change-zoom', (event, message) => {
  * @param callback
  */
 function setNotificationCallback(callback) {
-
     const OldNotify = window.Notification;
     const newNotify = (title, opt) => {
         callback(title, opt);
@@ -48,25 +52,104 @@ function clickSelector(element) {
     element.dispatchEvent(mouseEvent);
 }
 
+process.once('document-start', () => {
+    var Mousetrap = require('mousetrap')
 
-// (function(){
-//   // var root = document.documentElement;
-//   // root.classList.add('electron-client');
-//   document.addEventListener('DOMContentLoaded', function(){
-//
-//     // Say hi to our new friends.
-//     // window.Notification('Did you know?', {
-//     //   body: 'If we added web notifications they would appear natively here? (also, they would be pre-approved)'
-//     // });
-//
-//     //Added a class incase the app wants that...
-//     document.documentElement.classList.add('electron-client');
-//
-//     // Adding our own style overrides...
-//     var link = document.createElement("link");
-//     link.href = path.join('file://',__dirname, 'preload.css');
-//     link.type = "text/css";
-//     link.rel = "stylesheet";
-//     document.getElementsByTagName("head")[0].appendChild(link);
-//   }, false);
-// })();
+    // map multiple combinations to the same callback
+    Mousetrap.bind(['command+[', 'ctrl+['], () => {
+        document.querySelector('[data-auto-id="top-bar-menu-toggle"]').click();
+        // return false to prevent default behavior and stop event from bubbling
+        return false
+    })
+
+    // map multiple combinations to the same callback
+    Mousetrap.bind(['command+]', 'ctrl+]'], () => {
+        var selection = document.querySelector('[data-auto-id="top-bar-menu-toggle"]'); 
+        if(selection) {
+            selection.click();
+        }
+        // return false to prevent default behavior and stop event from bubbling
+        return false
+    })
+
+    Mousetrap.bind('up', () => {
+        var selection = document.querySelector('.item-iterator a[title="Previous"]');
+        if(selection) {
+            selection.click();
+        }
+    })
+
+    Mousetrap.bind('down', () => {
+        var selection = document.querySelector('.item-iterator a[title="Next"]');
+        if(selection) {
+            selection.click();
+        }
+    })
+
+    window.addEventListener('storage', function(e) {
+        updateToken();
+    });
+
+    function updateToken(){
+        var sessionItem = JSON.parse(localStorage.getItem(['ember_simple_auth-session']));
+        if (sessionItem && sessionItem.authenticated) {
+            var  token = null; // logged out by default
+            if (sessionItem.authenticated.token) {
+                // if session exists
+                // set token
+                token = sessionItem.authenticated.token;
+            }
+
+            if(token != store.get('token')) {
+                store.set('token', token);
+                updateUserTraits(token);
+            }
+        }
+    }
+
+    function updateUserTraits(token = ''){
+        if (token) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", `https://${API_HOST}/users/me?include=soapbox`, true);
+            xhr.setRequestHeader('Authorization','Bearer ' + token);
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState == 4) {
+                var resp = JSON.parse(xhr.responseText);
+                var firstIncluded = resp && resp.included ? resp.included[0] : null;
+                var attrs = firstIncluded ? firstIncluded.attributes : null;
+                var slug = attrs ? attrs.slug : null;
+  
+                if (slug) {
+                  var soapboxUrl  = slug + "." + BASE_DOMAIN;
+                  store.set('soapboxUrl', soapboxUrl);
+                  store.set('me', resp);
+                }
+              }
+            }
+            xhr.send();
+        }
+    }
+
+});
+
+(function(){
+  // var root = document.documentElement;
+  // root.classList.add('electron-client');
+  document.addEventListener('DOMContentLoaded', function(){
+
+    // Say hi to our new friends.
+    // window.Notification('Did you know?', {
+    //   body: 'If we added web notifications they would appear natively here? (also, they would be pre-approved)'
+    // });
+
+    //Added a class incase the app wants that...
+    document.documentElement.classList.add('electron-client');
+
+    // Adding our own style overrides...
+    var link = document.createElement("link");
+    link.href = path.join('file://',__dirname, 'preload.css');
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }, false);
+})();
